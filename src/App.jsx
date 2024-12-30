485b7fcfb681a584d57c56ae949428df344e676a
ThunderRabbit <zuzazibubabi@gmail.com>
2024-12-30 20:36:35 +0800

    added the add to wishlist function

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,29 @@ import "./App.css";
 function App() {
   const [bookCollection, setBookCollection] = useState([]);
   const [input, setInput] = useState("");
+
+  const [user, setUser] = useState({
+    name: "Carl Landicho",
+    myBooks: [],
+    wishlist: [],
+    completed: [],
+  });
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(input);
 
     try {
       const response = await fetch(
-        `https://openlibrary.org/search.json?title=${input}&limit=2`
+        `https://openlibrary.org/search.json?title=${input}&limit=4`
       );
       const data = await response.json();
       console.log(data.docs);
       const newBooks = data.docs.map((book) => {
         return {
           title: book.title,
-          bookImage: `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`,
-          ratings_averagew: book.ratings_average,
+          image: `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`,
+          ratings_average: book.ratings_average,
         };
       });
       setBookCollection(newBooks);
@@ -31,20 +39,74 @@ function App() {
     setInput(e.target.value);
   };
 
+  const addToWishList = (book) => {
+    setUser((prev) => ({
+      ...prev,
+      wishlist: [...prev.wishlist, book],
+    }));
+  };
+
+  const [showWishlist, setShowWishlist] = useState(false);
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={handleChange} />
-        <button>search</button>
-      </form>
-      <div className="bookList">
-        {bookCollection.length > 0 && (
-          <ul>
-            {bookCollection.map((book, index) => {
-              return <li key={index}>{book.title}</li>;
-            })}
-          </ul>
-        )}
+      <div className="App">
+        <div className="sideBar">
+          <div className="userAccount">Carl Landicho</div>
+          <div className="userNavigation">
+            <div className="myBooks">My Books</div>
+            <div
+              className="wishList"
+              onClick={() => setShowWishlist(!showWishlist)}
+            >
+              Wishlist
+            </div>
+            <div className="completed">Completed</div>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={input} onChange={handleChange} />
+          <button>search</button>
+        </form>
+        <div className="bookList">
+          {bookCollection.length > 0 && showWishlist == false && (
+            <ul>
+              {bookCollection.map((book, index) => {
+                return (
+                  <li key={index}>
+                    <h3>{book.title}</h3>
+                    <img src={book.image} />
+                    <button
+                      className="addToWishlist"
+                      onClick={() => {
+                        addToWishList(book);
+                      }}
+                    >
+                      Wishlist
+                    </button>
+                    <p>
+                      Ratings: {book.ratings_average || "Ratings not available"}
+                    </p>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
+        <div className="userWishlist">
+          {showWishlist && (
+            <ul>
+              {user.wishlist.map((book, index) => {
+                return (
+                  <li key={index}>
+                    <h3>{book.title}</h3>
+                    <img src={book.image} />
+                    <p>{book.ratings_average}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </div>
       </div>
     </>
   );
