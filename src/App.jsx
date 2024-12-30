8ca3ad055be4e974e4cc73e4828dd822ac5112a5
ThunderRabbit <zuzazibubabi@gmail.com>
2024-12-30 14:00:43 +0800

    learned to display the results in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [bookCollection, setBookCollection] = useState([]);
   const [input, setInput] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -9,10 +10,18 @@ function App() {
 
     try {
       const response = await fetch(
-        `https://openlibrary.org/search.json?title=${input}`
+        `https://openlibrary.org/search.json?title=${input}&limit=2`
       );
       const data = await response.json();
       console.log(data.docs);
+      const newBooks = data.docs.map((book) => {
+        return {
+          title: book.title,
+          bookImage: `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`,
+          ratings_averagew: book.ratings_average,
+        };
+      });
+      setBookCollection(newBooks);
     } catch (error) {
       console.error(error);
     }
@@ -28,6 +37,15 @@ function App() {
         <input type="text" value={input} onChange={handleChange} />
         <button>search</button>
       </form>
+      <div className="bookList">
+        {bookCollection.length > 0 && (
+          <ul>
+            {bookCollection.map((book, index) => {
+              return <li key={index}>{book.title}</li>;
+            })}
+          </ul>
+        )}
+      </div>
     </>
   );
 }
